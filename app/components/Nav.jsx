339aa25a2e17fbfe349f8921e01f60ca89ed49ab
lakshman4th <lakshman.4thpointer@gmail.com>
2017-02-20 11:35:21 +0530

    change in design

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,11 +1,13 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
+var {Button} = require('react-bootstrap');
 
 var Nav = React.createClass({
   render: function () {
     return (
       <div>
         <h2>Nav Component</h2>
+        <Button color="danger">sdfsdf</Button>
         <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
         <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
         <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
